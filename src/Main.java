b78bfc76a49d03c8ee848f6a5b04777c4ef7ab86
Michail-lsk <m.liskovcev@gmail.com>
2022-11-24 23:04:32 +0400

    1

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+import MyCollection.Collection;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Collection humans = new Collection();
+
+        humans.add("Igor");
+        humans.add("Viktor");
+//        humans.add("Gena");
+//        humans.add("Loh");
+//        humans.add("Vitalya");
+//        humans.add("Maks");
+//        humans.add("Vano");
+//        humans.add("Luda");
+//        humans.add("Katya");
+//        humans.add("Kiruha");
+//        humans.add("Pisun");
+
+        humans.erase(1);
+
+
+
+        for (int i = 0; i < humans.getSize();i++) {
+            System.out.println(humans.getName(i));
+        }
+
+        System.out.println(humans.getSize());
+        System.out.println(humans.getLength());
+    }
+
+}
